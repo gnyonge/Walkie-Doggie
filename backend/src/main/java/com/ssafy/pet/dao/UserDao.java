d574867abe4584e4383a415936b357402e71a0ee
김경연 <ghwoer12@naver.com>
2021-04-26 13:33:14 +0900

    [backend] 기능추가 : 회원가입

--- backend/src/main/java/com/ssafy/pet/dao/UserDao.java
+++ backend/src/main/java/com/ssafy/pet/dao/UserDao.java
@@ -2,7 +2,13 @@ package com.ssafy.pet.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.ssafy.pet.dto.UserDto;
+
 @Mapper
 public interface UserDao {
+	
+	int signup(UserDto user);
+	
+	int check_nick(String nick);
 
 }
