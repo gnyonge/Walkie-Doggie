83229ad56eba7c26fa102b51f54336f66465a989
김경연 <ghwoer12@naver.com>
2021-05-07 09:50:43 +0900

    [backend] 기능수정;로그인

--- backend/src/main/java/com/ssafy/pet/dao/UserDao.java
+++ backend/src/main/java/com/ssafy/pet/dao/UserDao.java
@@ -1,9 +1,11 @@
 package com.ssafy.pet.dao;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.ssafy.pet.dto.PetDto;
 import com.ssafy.pet.dto.UserDto;
 
 @Mapper
@@ -14,6 +16,7 @@ public interface UserDao {
 	int check_nick(String nick);
 	
 	UserDto checkEmail(String email);
+	List<PetDto> petInfo(String uid);
 		
 //	int authEmail(Map<String, Object> map);
 
--- backend/src/main/java/com/ssafy/pet/util/JWTUtil.java
+++ backend/src/main/java/com/ssafy/pet/util/JWTUtil.java
@@ -20,31 +20,33 @@ import io.jsonwebtoken.SignatureAlgorithm;
 public class JWTUtil {
 	public static final Logger logger = LoggerFactory.getLogger(JWTUtil.class);
 
-	private static final String TK = "WALKIEDOGGIE";
-	private static final long EXPIRE_MINUTES = 30 * 60 * 1000L; //30분
+	private static final String TK = "WALKIEDOGGIETOKEN";
+	private static final long EXPIRE_MINUTES = 30 * 60 * 1000L; // 30분
 
+	// 로그인 성공시 사용자 정보를 기반으로 JWTToken을 생성하여 반환.
+	// JWT Token = Header + Payload + Signature
 	public String create(UserDto user) {
-	     JwtBuilder jwtBuilder = Jwts.builder();
+		JwtBuilder jwtBuilder = Jwts.builder();
 
-	        // Header 설정
-	        jwtBuilder.setHeaderParam("typ", "JWT"); // 토큰의 타입으로 고정 값.
+		// Header 설정
+		jwtBuilder.setHeaderParam("typ", "JWT"); // 토큰의 타입으로 고정 값.
 
-	        // Payload 설정
-	        jwtBuilder
-	            .setSubject("access-token") // 토큰의 제목 설정
-	            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * EXPIRE_MINUTES)) // 유효기간 설정
-	            .claim("user", user) //정보 저장
-	            .claim("greeting", "환영합니다. " + user.getU_nickname()); // 담고 싶은 정보 설정.
+		// Payload 설정 - claim 정보 포함
+		jwtBuilder.setSubject("doggie_token") // 토큰의 제목 설정
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * EXPIRE_MINUTES)) // 유효기간
+				.claim("user", user) // 정보 저장
+				.claim("message", "환영합니다. " + user.getU_nickname()); // 담고 싶은 정보 설정.
 
-	        // signature 설정
-	        jwtBuilder.signWith(SignatureAlgorithm.HS256,this.generateKey());
+		// signature 설정 - secret key를 이용한 암호화
+		jwtBuilder.signWith(SignatureAlgorithm.HS256, this.generateKey());
 
-	        // 마지막 직렬화 처리
-	        String jwt = jwtBuilder.compact();
-	        logger.info("jwt : {}", jwt);
-	        return jwt;
+		// 마지막 직렬화 처리
+		String jwt = jwtBuilder.compact();
+		logger.info("jwt : {}", jwt);
+		return jwt;
 	}
-	
+
+	//TK를 getBytes() 처리
 	private byte[] generateKey() {
 		byte[] key = null;
 		try {
@@ -59,41 +61,42 @@ public class JWTUtil {
 
 		return key;
 	}
-	
+
 	// 전달 받은 토큰이 제대로 생성된것이니 확인 하고 문제가 있다면 RuntimeException을 발생.
-    public void checkValid(String jwt) {
-    	// 예외가 발생하지 않으면 OK
-        Jwts.parser().setSigningKey(this.generateKey()).parseClaimsJws(jwt);
-    }
-
-    // JWT Token을 분석해서 필요한 정보를 반환.
-    public Map<String, Object> get(String jwt) {
-        Jws<Claims> claims = null;
-        try {
-            claims = Jwts.parser().setSigningKey(this.generateKey()).parseClaimsJws(jwt);
-        } catch (final Exception e) {
-            throw new RuntimeException();
-        }
-
-    logger.info("claims : {}", claims);
-    // Claims는 Map의 구현체이다.
-    return claims.getBody();
+	//JWTinterceptor에서 사용!
+	public void checkValid(String jwt) {
+		// 예외가 발생하지 않으면 OK
+		logger.info("토큰 점검: {}", jwt);
+		Jwts.parser().setSigningKey(this.generateKey()).parseClaimsJws(jwt);
 	}
-    
-    public String getUserId(String jwt) {
-    	Jws<Claims> claims = null;
-        try {
-            claims = Jwts.parser().setSigningKey(this.generateKey()).parseClaimsJws(jwt);
-        } catch (final Exception e) {
-            throw new RuntimeException();
-        }
-        
-        Map<String, Object> value = claims.getBody();
+
+	// JWT Token을 분석해서 필요한 정보를 반환.
+	public Map<String, Object> get(String jwt) {
+		Jws<Claims> claims = null;
+		try {
+			claims = Jwts.parser().setSigningKey(this.generateKey()).parseClaimsJws(jwt);
+		} catch (final Exception e) {
+			throw new RuntimeException();
+		}
+
+		logger.info("claims : {}", claims);
+		// Claims는 Map의 구현체이다.
+		return claims.getBody();
+	}
+	
+	public String getUserId(String jwt) {
+		Jws<Claims> claims = null;
+		try {
+			claims = Jwts.parser().setSigningKey(this.generateKey()).parseClaimsJws(jwt);
+		} catch (final Exception e) {
+			throw new RuntimeException();
+		}
+
+		Map<String, Object> value = claims.getBody();
 		logger.info("value : {}", value);
-		
+
 		return (String) value.get("uid");
-    	
-    }
-	
-	
+		
+	}
+
 }
