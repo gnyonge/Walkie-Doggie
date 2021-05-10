313c274d6197bb6c61439e4203be2e74c27559f3
gnyonge <gnyonge@gmail.com>
2021-05-10 16:27:40 +0900

    axios 추가

--- frontend/final/src/store/index.js
+++ frontend/final/src/store/index.js
@@ -2,32 +2,18 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate';
 
+import calendar from './module/calendarStore';
+import walk from './module/walkStore';
+import mypage from './module/mypageStore';
+import food from './module/foodStore';
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: { calendar, walk, mypage, food },
   plugins: [
-    createPersistedState()
+    createPersistedState({
+      paths: ['calendar', 'walk', 'mypage', 'food']
+    })
   ],
-  state: {
-    nowTab: 0,
-    selectedDate: ""
-  },
-  getters: {
-    getNowTab(state) {
-      return state.nowTab
-    },
-    getSelectedDate(state) {
-      return state.selectedDate
-    }
-  },
-  mutations: {
-    setNowTab(state, now) {
-      state.nowTab = now
-    },
-    setSelectedDate(state, date) {
-      state.selectedDate = date
-    }
-  },
-  actions: {
-  },
 })
