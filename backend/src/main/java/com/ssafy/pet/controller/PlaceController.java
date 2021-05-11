b1912029f5cb1f2c3702277ddbde71567c1e5275
윤수민 <sumin9497@naver.com>
2021-05-11 21:43:30 +0900

    [backend]기능추가: 좋아요 게시글 이미지 업로드 별도로 빼기

--- backend/src/main/java/com/ssafy/pet/controller/PlaceController.java
+++ backend/src/main/java/com/ssafy/pet/controller/PlaceController.java
@@ -52,45 +52,73 @@ public class PlaceController {
 	@Autowired
     private PlaceService placeService;
 
+    /*
+     * 기능: 산책 중 특정 장소 좋아요 클릭
+     * 
+     * developer: 윤수민
+     * 
+     * @param file
+     * 
+     * @return message, url
+	 * 
+     */
+    @ApiOperation(value = "Like place/ Insert HotPlace image", notes = "좋아요 이미지 업로드")
+    @PostMapping("/imageUpload")
+    public ResponseEntity<Map<String, Object>> imageUpload( MultipartFile file) {
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+        
+        try {
+			logger.info("place/imageUpload 호출 성공");
+
+            String originName = file.getOriginalFilename(); // 파일 이름 가져오기
+
+            String ext = originName.substring(originName.lastIndexOf('.')); // 파일 확장명 가져오기
+            String saveFileName = UUID.randomUUID().toString() + ext; // 암호화해서 파일확장넣어주기
+            String path = System.getProperty("user.dir"); // 경로설정해주고
+
+            File tempfile = new File(path, saveFileName); // 경로에 파일만들어주고
+
+            String line = "diary/";
+
+            saveFileName = line + saveFileName;
+
+            file.transferTo(tempfile);
+            s3util.setS3Client().putObject(new PutObjectRequest(bucket, saveFileName, tempfile)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+            String url = s3util.setS3Client().getUrl(bucket, saveFileName).toString();
+            tempfile.delete();
+
+            resultMap.put("url", url);
+            resultMap.put("message", "이미지 업로드 성공하였습니다.");
+			status = HttpStatus.ACCEPTED;
+			
+        } catch (Exception e) {
+            logger.error("이미지 업로드 실패 : {}", e);
+			resultMap.put("message", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+
 	/*
      * 기능: 산책 중 특정 장소 좋아요 클릭
      * 
      * developer: 윤수민
      * 
-     * @param p_latitude, p_longtitude, peid, uid, l_desc, file
+     * @param p_latitude, p_longtitude, peid, l_desc, l_image
      * 
      * @return message
 	 * 
      */
     @ApiOperation(value = "Like place/ Insert HotPlace post", notes = "산책 중 특정 장소 좋아요 클릭")
     @PostMapping("/likePlace")
-    public ResponseEntity<Map<String, Object>> likePlace(@RequestPart MultipartFile file, @RequestPart Map<String, Object> param) {
+    public ResponseEntity<Map<String, Object>> likePlace( @RequestBody Map<String, Object> param) {
         Map<String, Object> resultMap = new HashMap<>();
         HttpStatus status = null;
         
         try {
 			logger.info("place/likePlace 호출 성공");
-            
-            if(file != null){
-				String originName = file.getOriginalFilename(); // 파일 이름 가져오기
-
-				String ext = originName.substring(originName.lastIndexOf('.')); // 파일 확장명 가져오기
-				String saveFileName = UUID.randomUUID().toString() + ext; // 암호화해서 파일확장넣어주기
-				String path = System.getProperty("user.dir"); // 경로설정해주고
-
-				File tempfile = new File(path, saveFileName); // 경로에 파일만들어주고
-
-				String line = "diary/";
-
-				saveFileName = line + saveFileName;
-
-				file.transferTo(tempfile);
-				s3util.setS3Client().putObject(new PutObjectRequest(bucket, saveFileName, tempfile)
-						.withCannedAcl(CannedAccessControlList.PublicRead));
-				String url = s3util.setS3Client().getUrl(bucket, saveFileName).toString();
-				tempfile.delete();
-				param.put("l_image", url);
-			}
 
 			// 등록된 장소인지 먼저 확인
 			Integer pid = placeService.checkPlace(param); 
