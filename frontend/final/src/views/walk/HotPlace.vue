c714683af16a04fe9865256e4e5c984c9bb03848
ekals4407 <ekals4407@naver.com>
2021-05-13 21:18:23 +0900

    [frontend] 도와줘요! 갓진영!

--- frontend/final/src/views/walk/HotPlace.vue
+++ frontend/final/src/views/walk/HotPlace.vue
@@ -26,25 +26,25 @@ export default {
       map: {},
       positions: [ // 일단 더미 데이터
         {
-          title: '마킹한 곳 ', 
+          title: '사진이 잘 나와요!', 
           lat: 35.17104466433188,
           lon: 126.80083721411596,
           imageSrc: "@/assets/diarydogs.jpg"
         },
         {
-          title: '생태연못', 
+          title: '휴식 공간이 있어요!', 
           lat: 35.17243177736109, 
           lon: 126.80249585904436,
           imageSrc: 'https://t1.daumcdn.net/liveboard/holapet/0e5f90af436e4c218343073164a5f657.JPG'
         },
         {
-          title: '텃밭', 
+          title: '마킹을 했어요!', 
           lat: 35.17378144379576,
           lon: 126.80402040469657,
           imageSrc: 'https://lh3.googleusercontent.com/proxy/DZtjLulmXwRts3LBz1d_t6tcvwDjL2RUt7kXXJS7bSplRfQDYT6RMEBmSdeb4q3WljgZqSQDnoDDaIQMHoR_odQQREu0kWFTnnipF0UMKdXfoR4jeCmPTdLB7YMDKwN3LPx1DYzmoeue-nwzAt_H6xcNCI-4ugdYYdtPUvYbIgM'
         },
         {
-          title: '왜 마지막만 나와',
+          title: '날씨가 좋아요!',
           lat: 35.17220631984472, 
           lon: 126.80086444891174,
           imageSrc: 'https://cbmpress.sfo2.digitaloceanspaces.com/vlife/1613865410_dmlhUINL_258ccfc67b9d31c9a6414eb0663033ea96d4be54.png'
