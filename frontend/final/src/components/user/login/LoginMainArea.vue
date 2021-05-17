724ab9278370557a77f6e65bc843c847fcb81236
김경연 <ghwoer12@naver.com>
2021-05-17 10:56:44 +0900

    [frontend] 카카오로그인

--- frontend/final/src/components/user/login/LoginMainArea.vue
+++ frontend/final/src/components/user/login/LoginMainArea.vue
@@ -23,11 +23,76 @@
 </template>
 
 <script>
+import { rscApi } from '@/services/api';
+
 export default {
+  data(){
+    return{
+      user:{
+        uid:"",
+        u_email:"",
+        u_location:"",
+        u_nickname:"",
+    },
+    }
+  },
   methods: {
     goto(path) {
       this.$router.push(path)
-    }
+    },
+    kakao(){
+            console.log(window.Kakao);
+      // const params={
+      //   redirectUri:"http://localhost:8080/login",
+      // };
+      window.Kakao.Auth.login({
+        scope:'account_email, profile',
+        success: this.GetMe,
+      });
+    },
+        GetMe(authObj){
+      console.log(authObj);
+      window.Kakao.API.request({
+        url:'/v2/user/me',
+        success:res=>{
+          const kakao_account = res.kakao_account;
+          const userInfo={
+            nickname : kakao_account.profile.nickname,
+            email:kakao_account.email,
+          };
+          console.log(userInfo);
+          this.user.u_email = userInfo.email;
+          this.user.u_nickname = userInfo.nickname;
+          rscApi.post(`login/ksign`,this.user)
+                .then(({data}) => {
+                  this.user = data.user;
+                  if(this.user.u_location==null){
+                    //지역 등록 안했으니까 지역 등록으로
+                    this.$router.push("/register")
+                  }else{
+                    //지역 등록 했으니까
+                    if(data.list==null){
+                    this.$router.push("/dogregister")
+
+                    }else{
+                    this.$router.push('/calendar')
+                    }
+                  }
+                  console.log(data);
+                  
+    //     console.log(data);
+    //     // doggie_token을 cookie로 저장
+    //     this.$router.push(`/calendar`)
+      })
+                .catch (()=>{    
+          // this.$router.push(`/login`)
+
+      })
+          // this.$router.push('/calendar')
+        }
+      })
+  },
+
   },
 }
 </script>
