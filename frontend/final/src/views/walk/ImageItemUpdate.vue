812136e06faa65241e0e8d1bd5174ec416fbfba0
LynnYeonjuLee <lynnlyj9@gmail.com>
2021-05-18 09:34:01 +0900

    [frontend]수정삭제버튼 예외처리 및 수정페이지

---
+++ frontend/final/src/views/walk/ImageItemUpdate.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ImageItemUpdateArea />
+  </div>
+</template>
+
+<script>
+import ImageItemUpdateArea from "@/components/walk/ImageItemUpdateArea.vue";
+export default {
+  name: 'ImageItemUpdate',
+  components: {
+    ImageItemUpdateArea
+  },
+}
+</script>
